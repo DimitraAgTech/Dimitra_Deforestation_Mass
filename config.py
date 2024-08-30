ceec7055a938f0fb497319bf7ff1c44df3ca2225
Tushar Jain <tusharlock10@gmail.com>
2024-08-31 04:16:52 +0530

    added logs

--- config.py
+++ config.py
@@ -4,21 +4,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-HOST = os.getenv('HOST', None)
-DATABASE = os.getenv('DATABASE', None)
-DB_USERNAME = os.getenv('DB_USERNAME', None)
-PASSWORD = os.getenv('PASSWORD', None)
+print("ENV VARIABLES :: ", dict(os.environ))
 
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', None)
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', None)
-AWS_REGION = os.getenv('AWS_REGION', None)
-S3_RESULT_BUCKET = os.getenv('S3_RESULT_BUCKET', None)
+HOST = os.getenv("HOST", None)
+DATABASE = os.getenv("DATABASE", None)
+DB_USERNAME = os.getenv("DB_USERNAME", None)
+PASSWORD = os.getenv("PASSWORD", None)
 
-SYNC_KEY = os.getenv('SYNC_KEY', None)
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", None)
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", None)
+AWS_REGION = os.getenv("AWS_REGION", None)
+S3_RESULT_BUCKET = os.getenv("S3_RESULT_BUCKET", None)
+
+SYNC_KEY = os.getenv("SYNC_KEY", None)
 
 DATABASE_URI = f"mysql+pymysql://{DB_USERNAME}:{PASSWORD}@{HOST}/{DATABASE}"
 
-DEFORESTATION_API = os.getenv('DEFORESTATION_API', None)
-NODE_CALLBACK_URL = os.getenv('NODE_CALLBACK_URL', None)
+DEFORESTATION_API = os.getenv("DEFORESTATION_API", None)
+NODE_CALLBACK_URL = os.getenv("NODE_CALLBACK_URL", None)
 
-LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
