15b478e6a06807d25615ab2b08ed73b935ccbfb1
Tushar Jain <tusharlock10@gmail.com>
2024-09-01 14:56:24 +0530

    feat: add completion timestamp

--- database/models.py
+++ database/models.py
@@ -7,7 +7,7 @@ from database import db
 
 
 class DeforestationRequest(db.Base):
-    __tablename__ = 'deforestation_request'
+    __tablename__ = "deforestation_request"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     status = Column(String(30), nullable=False)
@@ -16,3 +16,4 @@ class DeforestationRequest(db.Base):
     is_synced = Column(Boolean, nullable=False, default=False)
     error = Column(String, nullable=True, default=None)
     timestamp = Column(DateTime, nullable=False)
+    completion_timestamp = Column(DateTime, nullable=True)
