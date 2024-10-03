432df92f7edf8a4f26342be8d146d37397a50e28
Tushar Jain <tusharlock10@gmail.com>
2024-10-03 19:18:33 +0530

    feat: add geofence info task

--- database/models.py
+++ database/models.py
@@ -14,6 +14,20 @@ class DeforestationRequest(db.Base):
     total = Column(Integer, nullable=False)
     completed = Column(Integer, nullable=False, default=0)
     is_synced = Column(Boolean, nullable=False, default=False)
+    is_pdf_report = Column(Boolean, nullable=False, default=False)
+    error = Column(String, nullable=True, default=None)
+    timestamp = Column(DateTime, nullable=False)
+    completion_timestamp = Column(DateTime, nullable=True)
+
+
+class GeofenceInfoRequest(db.Base):
+    __tablename__ = "geofence_info_request"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    status = Column(String(30), nullable=False)
+    total = Column(Integer, nullable=False)
+    completed = Column(Integer, nullable=False, default=0)
+    is_synced = Column(Boolean, nullable=False, default=False)
     error = Column(String, nullable=True, default=None)
     timestamp = Column(DateTime, nullable=False)
     completion_timestamp = Column(DateTime, nullable=True)
